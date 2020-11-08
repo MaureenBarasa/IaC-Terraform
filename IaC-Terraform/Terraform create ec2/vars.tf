86ed9979f9ecc6a7271ee4be2cbdb22d658ccc74
Maureen Barasa <47135007+MaureenBarasa@users.noreply.github.com>
2020-11-08 23:47:17 +0300

    Update vars.tf

--- IaC-Terraform/Terraform create ec2/vars.tf
+++ IaC-Terraform/Terraform create ec2/vars.tf
@@ -1,9 +1,9 @@
 variable "AWS_ACCESS_KEY" {
-  default = "AKIAWID43R4A757FKC52"
+  default = "****************"
 }
 
 variable "AWS_SECRET_KEY" {
-  default = "7OFHjjPck/s9SG4WL2t00WMtsY1vbIZI9F0sJPJD"
+  default = "*********************"
 }
 
 variable "AWS_REGION" {
