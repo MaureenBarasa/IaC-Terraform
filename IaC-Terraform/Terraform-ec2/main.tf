f8568d610eafde7c8db809d81e459211818063cd
Maureen Barasa <47135007+MaureenBarasa@users.noreply.github.com>
2020-11-09 01:19:02 +0300

    create main.tf
    initial commit

---
+++ IaC-Terraform/Terraform-ec2/main.tf
@@ -0,0 +1,15 @@
+variable "AWS_ACCESS_KEY" {
+  default = "**************************"
+}
+
+variable "AWS_SECRET_KEY" {
+  default = "**************************"
+}
+
+variable "AWS_REGION" {
+  default = "eu-central-1"
+}
+
+variable "key_name" {
+  default = "test-key"
+}
