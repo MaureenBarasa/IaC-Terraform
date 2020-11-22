7d88408e910bc5249fe451bf74c2ed0e2fb3ebd2
Maureen Barasa <47135007+MaureenBarasa@users.noreply.github.com>
2020-11-22 20:51:24 +0300

    Update outputs.tf
    added outputs for alb security group

--- IaC-Terraform/modules/services/vpc/outputs.tf
+++ IaC-Terraform/modules/services/vpc/outputs.tf
@@ -8,8 +8,28 @@ output "vpc_public_subnet2" {
   value       = "${aws_subnet.test-public-subnet-2.id}"
 }
 
+output "vpc_private_subnet1_id" {
+  description = "ID of the VPC's private subnets"
+  value       = "${aws_subnet.test-private-subnet-1.id}"
+}
+
+output "vpc_private_subnet2_id" {
+  description = "ID of the VPC's private subnets"
+  value       = "${aws_subnet.test-private-subnet-2.id}"
+}
+
 output "vpc_security_group_id" {
-  description = "IDs of the VPC's security groups"
+  description = "ID of the VPC's security groups"
   value       = "${aws_security_group.test-ec2-sg.id}"
+}
+
+output "vpc_ecs_security_group_id" {
+  description = "ID of the ECS security groups"
+  value       = "${aws_security_group.test-ecs-ec2-sg.id}"
+}
+
+output "vpc_ecs_security_group_id" {
+  description = "ID of the ALB security groups"
+  value       = "${aws_security_group.test-alb-sg.id}"
 }
 
