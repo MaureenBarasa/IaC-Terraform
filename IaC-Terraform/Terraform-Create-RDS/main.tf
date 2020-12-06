f9a1ebd8c9e193d28b3fd02ae2861995f2bc6085
Maureen Barasa <47135007+MaureenBarasa@users.noreply.github.com>
2020-12-06 16:42:16 +0300

    Update main.tf
    updated main.tf with RDS module

--- IaC-Terraform/Terraform-Create-RDS/main.tf
+++ IaC-Terraform/Terraform-Create-RDS/main.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
+  region = "eu-central-1"
+}
+
+module "vpc" {
+  source  = "/Users/maureenbarasa/modules/services/vpc"
+}
+
 module "RDS" {
   source  = "/Users/maureenbarasa/modules/services/RDS"
 }
