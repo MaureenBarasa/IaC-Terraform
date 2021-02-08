0c9966853f0f328760fc3e035a95bc45acc7fb94
Maureen Barasa <47135007+MaureenBarasa@users.noreply.github.com>
2021-02-08 20:42:28 +0300

    Update vars.tf
    removed credentials

--- XR3-Engine-Terraform/vars.tf
+++ XR3-Engine-Terraform/vars.tf
@@ -1,9 +1,9 @@
 variable "AWS_ACCESS_KEY" {
-  default = "AKIARQM6EGKHN7R7DSNO"
+  default = 
 }
 
 variable "AWS_SECRET_KEY" {
-  default = "SZsKm2ei8f23AXDvLNzhi3IISmLs2NFXXeaZFy6j"
+  default =
 }
 
 variable "AWS_REGION" {
