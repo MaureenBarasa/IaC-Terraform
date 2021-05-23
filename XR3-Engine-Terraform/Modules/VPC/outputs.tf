f775d129230e5a9c2b875cf7e2f2015c340bf0c2
Maureen Barasa <47135007+MaureenBarasa@users.noreply.github.com>
2021-05-23 13:02:02 +0300

    Update outputs.tf
    modified resource names

--- XR3-Engine-Terraform/Modules/VPC/outputs.tf
+++ XR3-Engine-Terraform/Modules/VPC/outputs.tf
@@ -1,36 +1,35 @@
 output "VPC_vpc_id" {
   description = "IDs of the VPC's public subnets"
-  value       = "${aws_vpc.XR3-VPC.id}"
+  value       = "${aws_vpc.SuperReality-VPC.id}"
 }
 
 output "VPC_public_subnet1_id" {
   description = "IDs of the VPC's public subnets"
-  value       = "${aws_subnet.XR3-PublicSub01.id}"
+  value       = "${aws_subnet.SuperReality-PublicSub01.id}"
 }
 
 output "VPC_public_subnet2_id" {
   description = "IDs of the VPC's public subnets"
-  value       = "${aws_subnet.XR3-PublicSub02.id}"
+  value       = "${aws_subnet.SuperReality-PublicSub02.id}"
 }
 
 output "VPC_private_subnet1_id" {
   description = "ID of the VPC's private subnets"
-  value       = "${aws_subnet.XR3-PrivateSub01.id}"
+  value       = "${aws_subnet.SuperReality-PrivateSub01.id}"
 }
 
 output "VPC_private_subnet2_id" {
   description = "ID of the VPC's private subnets"
-  value       = "${aws_subnet.XR3-PrivateSub02.id}"
+  value       = "${aws_subnet.SuperReality-PrivateSub02.id}"
 }
 
 output "VPC_private_subnet3_id" {
   description = "ID of the VPC's private subnets"
-  value       = "${aws_subnet.XR3-PrivateSub03.id}"
+  value       = "${aws_subnet.SuperReality-PrivateSub03.id}"
 }
 
 output "VPC_private_subnet4_id" {
   description = "ID of the VPC's private subnets"
-  value       = "${aws_subnet.XR3-PrivateSub04.id}"
+  value       = "${aws_subnet.SuperReality-PrivateSub04.id}"
 }
-
 
