41aafb81c2eb002072460164f99845d3cecbe29d
Maureen Barasa <47135007+MaureenBarasa@users.noreply.github.com>
2021-05-23 12:58:00 +0300

    Delete XR3-Engine-Terraform/Modules/EKS-Cluster-Managed-Nodes directory
    deleted directory

--- XR3-Engine-Terraform/Modules/EKS-Cluster-Managed-Nodes/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "eks_cluster_endpoint" {
-  value = aws_eks_cluster.xr3-engine-eks.endpoint
-}
-
-output "eks_cluster_certificate_authority" {
-  value = aws_eks_cluster.xr3-engine-eks.certificate_authority 
-}
