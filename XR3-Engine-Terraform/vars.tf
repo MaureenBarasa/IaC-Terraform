8eb52471096de3eac324fe9d978de38604459fcb
Maureen Barasa <47135007+MaureenBarasa@users.noreply.github.com>
2021-05-23 12:52:36 +0300

    Update vars.tf
    added DB root and user password as variables

--- XR3-Engine-Terraform/vars.tf
+++ XR3-Engine-Terraform/vars.tf
@@ -11,5 +11,27 @@ variable "AWS_REGION" {
 }
 
 variable "key_name" {
-  default = "Gaming"
+  default = "Your AWS Acccount Key Name"
+}
+
+variable "AWS_REGION" {
+  default = "Your AWS Account Region"
+}
+
+variable "key_name" {
+  default = "Your AWS Account EC2 Key Pair name"
+}
+
+variable "db_root_password" {
+  description = "the db root password"
+  type = "string"
+  default = "************"
+  sensitive = true
+}
+
+variable "db_user_password" {
+  description = "the db user password"
+  type = "string"
+  default = "**************"
+  sensitive = true
 }
