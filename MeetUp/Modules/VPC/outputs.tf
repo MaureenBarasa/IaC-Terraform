e2a35e520e9d5de1d56b72dc7dffaa04dad40c11
Maureen Barasa <47135007+MaureenBarasa@users.noreply.github.com>
2021-05-29 11:20:35 +0300

    added file outputs.tf

---
+++ MeetUp/Modules/VPC/outputs.tf
@@ -0,0 +1,24 @@
+output "VPC_vpc_id" {
+  description = "IDs of the VPC's public subnets"
+  value       = "${aws_vpc.SuperReality-VPC.id}"
+}
+
+output "VPC_public_subnet1_id" {
+  description = "IDs of the VPC's public subnets"
+  value       = "${aws_subnet.SuperReality-PublicSub01.id}"
+}
+
+output "VPC_public_subnet2_id" {
+  description = "IDs of the VPC's public subnets"
+  value       = "${aws_subnet.SuperReality-PublicSub02.id}"
+}
+
+output "VPC_private_subnet1_id" {
+  description = "ID of the VPC's private subnets"
+  value       = "${aws_subnet.SuperReality-PrivateSub01.id}"
+}
+
+output "VPC_private_subnet2_id" {
+  description = "ID of the VPC's private subnets"
+  value       = "${aws_subnet.SuperReality-PrivateSub02.id}"
+}
