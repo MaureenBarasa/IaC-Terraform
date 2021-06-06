929ad3bfbe3e3d718bb92adbe805dead25b5113d
Maureen Barasa <47135007+MaureenBarasa@users.noreply.github.com>
2021-06-06 19:19:23 +0300

    Update vars.tf
    removed unnecessary variables

--- XR3-Engine-Terraform/vars.tf
+++ XR3-Engine-Terraform/vars.tf
@@ -9,11 +9,6 @@ variable "AWS_SECRET_KEY" {
 variable "AWS_REGION" {
   default = "Your AWS Account Region"
 }
-
-variable "key_name" {
-  default = "Your AWS Account EC2 Key Pair name"
-}
-
 variable "db_root_password" {
   description = "the db root password"
   type = "string"
